fe13f3caf69a8a6a107981ffa8cbdec3e63fd535
Thord Setsaas <t.setsaas@sportradar.com>
2022-04-09 13:37:12 +0200

    Added new kcapp-sio-updater script

---
+++ kcapp-sio-updater.js
@@ -0,0 +1,37 @@
+const debug = require('debug')('kcapp-sio-updater');
+const axios = require('axios');
+
+const KCAPP_HOST = process.env.KCAPP_HOST || "localhost";
+const KCAPP_PORT = process.env.KCAPP_PORT || 3000;
+const KCAPP_PROTO = process.env.KCAPP_PROTO || "http";
+const ODDS_API = process.env.ODDS_API || "http://localhost:9001";
+
+const kcapp = require('kcapp-sio-client/kcapp')(KCAPP_HOST, KCAPP_PORT, "kcapp-announcer", KCAPP_PROTO);
+kcapp.connect(() => {
+    kcapp.on('warmup_started', (data) => {
+        const match = data.match;
+        debug(`warmup_started for match ${match.id}`);
+        axios.put(`${ODDS_API}/match/${match.id}/started`).then(response => {
+            debug(`Updated Odds-API with match started`);
+        })
+    });
+
+    kcapp.on('leg_finished', (data) => {
+        const match = data.match;
+        if (match.is_finished && match.tournament_id !== null) {
+            debug(`Match finished ${match.id}`);
+
+            const body = {
+                match_id: match.id,
+                winner_id: match.winner_id // Will be `null` on Draw
+                // Can also add homeWins and awayWins if we need it
+            };
+            axios.put(`${ODDS_API}/match/${match.id}/finished`, body).then(response => {
+                debug(`Updated Odds-API with match finish`);
+            })
+            .catch(error => {
+                debug(error);
+            });
+        }
+    });
+});
