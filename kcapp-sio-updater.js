215a5b5450266d8ae114630e063b8afb826adf70
Thord Setsaas <t.setsaas@sportradar.com>
2022-04-12 08:34:52 +0200

    Using .env files for kcapp-sio-updater

--- kcapp-sio-updater.js
+++ kcapp-sio-updater.js
@@ -3,37 +3,24 @@ import debugLib from "debug";
 import kcappLib from "kcapp-sio-client/kcapp.js";
 import dotenv from "dotenv";
 
-let VITE_ODDS_API = "";
-let VITE_KCAPP_SOCKET = "";
-let VITE_KCAPP_SOCKET_PORT = "";
-
-// use MODE variable to define which .env file to load
-// MODE=production node kcapp-sio-updater.js <= loads .env.production
-// MODE=production node kcapp-sio-updater.js <= loads .env.development
-
-if (process.env.MODE !== undefined) {
-  dotenv.config({ path: ".env." + process.env.MODE });
-  VITE_KCAPP_SOCKET = process.env.VITE_KCAPP_SOCKET;
-  VITE_KCAPP_SOCKET_PORT = process.env.VITE_KCAPP_SOCKET_PORT;
-  VITE_ODDS_API = process.env.VITE_ODDS_API;
-}
-
 const debug = debugLib("kcapp-sio-updater");
 
-const KCAPP_HOST = VITE_KCAPP_SOCKET || "localhost";
-const KCAPP_PORT = VITE_KCAPP_SOCKET_PORT || 3000;
-const KCAPP_PROTO = process.env.KCAPP_PROTO || "http";
-const ODDS_API = VITE_ODDS_API || "http://localhost:9999";
+const MODE = process.env.MODE || "development"
+dotenv.config({ path: ".env." + MODE });
+debug(`Running in ${MODE} mode`);
 
-const kcapp = kcappLib(KCAPP_HOST, KCAPP_PORT, "kcapp-announcer", KCAPP_PROTO);
+const VITE_KCAPP_SOCKET = process.env.VITE_KCAPP_SOCKET;
+const VITE_KCAPP_SOCKET_PORT = process.env.VITE_KCAPP_SOCKET_PORT;
+const VITE_ODDS_API = process.env.VITE_ODDS_API;
+debug(`Connecting to ${VITE_KCAPP_SOCKET}`)
 
+const kcapp = kcappLib(VITE_KCAPP_SOCKET, VITE_KCAPP_SOCKET_PORT, "kcapp-announcer", "http");
 kcapp.connect(() => {
   kcapp.on("warmup_started", (data) => {
     const match = data.match;
     debug(`warmup_started for match ${match.id}`);
-    axios.post(`${ODDS_API}/games/${match.id}/start`).then((response) => {
+    axios.post(`${VITE_ODDS_API}/games/${match.id}/start`).then((response) => {
       debug(`Updated Odds-API with match started`);
-      console.info(`Updated Odds-API with match started`);
     });
   });
 
@@ -44,11 +31,10 @@ kcapp.connect(() => {
 
       const body = {
         match_id: match.id,
-        winner_id: match.winner_id ? match.winner_id : 0, // 0 if draw
-        // Can also add homeWins and awayWins if we need it
+        winner_id: match.winner_id ? match.winner_id : 0
       };
       axios
-        .post(`${ODDS_API}/games/${match.id}/finish`, body)
+        .post(`${VITE_ODDS_API}/games/${match.id}/finish`, body)
         .then((response) => {
           debug(`Updated Odds-API with match finish`);
         })
