acffe4dab59eb3f461af879f2c9d4555416a9bf6
Lukasz Chmielewski <l.chmielewski@sportradar.com>
2025-05-20 16:38:42 +0200

    Changed total style

--- src/components/TheRanking.vue
+++ src/components/TheRanking.vue
@@ -1,85 +1,65 @@
 <template>
-  <div class="m10">
-    <div class="gameDiv">
-      <div class="tournamentTitle">
-        Tournament Bets Ranking:
-        {{ this.tournament ? this.tournament.name : "Unknown" }}
-      </div>
-      <div class="pt20">
-        <table class="rankingTable">
-          <tr>
-            <td class="juicyGreen pr10">#</td>
-            <td class="juicyGreen">user</td>
-            <td class="juicyGreen txtC" colspan="3">
-              coins available (open bets)
-            </td>
-            <td class="juicyGreen txtC">bets placed</td>
-            <td class="juicyGreen txtR">total potential</td>
-            <td class="juicyGreen txtR">avg coins / bet</td>
-          </tr>
-          <tr v-for="(item, index) in this.ranking" v-bind:key="index">
-            <RankingItem>
-              <template #index>{{ index + 1 }}.</template>
-              <template #playerName>
-                {{ item.first_name }} {{ item.last_name }}
-              </template>
-              <template #coins>
-                {{ item.coins_available.toFixed(2) }}
-              </template>
-              <template #coinsInActiveBets>
-                {{ item.tournament_coins_open }}
-              </template>
-              <template #betsPlaced>
-                {{ item.bets_placed }}
-              </template>
-              <template #totalPotential>
-                {{
-                  (
-                    item.coins_available +
-                    item.tournament_coins_open +
-                    item.potential_winnings
-                  ).toFixed(2)
-                }}
-              </template>
-              <template #avgWin v-if="item.bets_closed > 0">
-                <span
-                  :class="{
-                    colPlus:
-                      (item.coins_available - 1000) / item.bets_closed > 0,
-                    colMinus:
-                      (item.coins_available - 1000) / item.bets_closed < 0,
-                  }"
-                >
-                  {{
-                    ((item.coins_available - 1000) / item.bets_closed).toFixed(
-                      2
-                    )
-                  }}
-                </span>
-              </template>
-              <template #avgWin v-else>
-                {{ (0).toFixed(2) }}
-              </template>
-            </RankingItem>
-          </tr>
-        </table>
+  <div class="mt-8 px-4">
+    <div class="w-full max-w-7xl mx-auto">
+      <div class="bg-gray-800 rounded-lg shadow-xl p-6">
+        <div class="flex flex-col mb-4">
+          <h2 class="text-lg font-semibold text-white bg-gray-700 px-4 py-2 rounded w-full">
+            Tournament Bets Ranking: {{ this.tournament ? this.tournament.name : "Unknown" }}
+          </h2>
+        </div>
+        <div class="overflow-x-auto">
+          <table class="w-full">
+            <thead>
+              <tr class="text-white">
+                <th class="py-1 text-left">#</th>
+                <th class="py-1 text-left">User</th>
+                <th class="py-1 text-right">Coins Available</th>
+                <th class="py-1 text-center">Open Bets</th>
+                <th class="py-1 text-center">Bets Placed</th>
+                <th class="py-1 text-right">Total Potential</th>
+                <th class="py-1 text-right">Avg Coins/Bet</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(item, index) in this.ranking" :key="index" class="border-t border-gray-700">
+                <td class="py-1 text-gray-400">{{ index + 1 }}.</td>
+                <td class="py-1 text-gray-400">
+                  {{ item.first_name }} {{ item.last_name }}
+                </td>
+                <td class="py-1 text-right" :class="{
+                  'text-red-500': item.coins_available < 0,
+                  'text-green-500': item.coins_available > 0,
+                  'text-gray-400': item.coins_available === 0
+                }">
+                  {{ item.coins_available.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).replace(/,/g, ' ') }}
+                </td>
+                <td class="py-1 text-center text-gray-400">{{ item.tournament_coins_open.toLocaleString('en-US', { maximumFractionDigits: 0 }).replace(/,/g, ' ') }}</td>
+                <td class="py-1 text-center text-gray-400">{{ item.bets_placed }}</td>
+                <td class="py-1 text-right text-gray-400">
+                  {{ (item.coins_available + item.tournament_coins_open + item.potential_winnings).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).replace(/,/g, ' ') }}
+                </td>
+                <td class="py-1 text-right" :class="{
+                  'text-red-500': item.bets_closed > 0 && (item.coins_available - 1000) / item.bets_closed < 0,
+                  'text-green-500': item.bets_closed > 0 && (item.coins_available - 1000) / item.bets_closed > 0,
+                  'text-gray-400': item.bets_closed === 0
+                }">
+                  {{ item.bets_closed > 0 ? ((item.coins_available - 1000) / item.bets_closed).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).replace(/,/g, ' ') : "0.00" }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import RankingItem from "@/components/RankingItem.vue";
 export default {
-  components: { RankingItem },
   props: ["tournament", "ranking"],
   data() {
     return {};
   },
-  methods: {
-    //getUserData(gameId) {},
-  },
-  mounted() {},
 };
 </script>
 
--- src/components/TheTotalRanking.vue
+++ src/components/TheTotalRanking.vue
@@ -1,53 +1,68 @@
 <template>
-  <div class="m10">
-    <div class="gameDiv">
-      <div class="tournamentTitle">
-        Tournament Match Bets Ranking:
-        {{ this.tournament ? this.tournament.name : "Unknown" }}
-      </div>
-      <div class="pt20">
-        <table class="rankingTable" v-if="ranking">
-          <tr>
-            <td class="juicyGreen pr10">#</td>
-            <td class="juicyGreen">user</td>
-            <td class="juicyGreen txtR">game coins won</td>
-            <td class="juicyGreen txtR">tournament coins won</td>
-            <td class="juicyGreen txtR">total coins won</td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr
-            v-for="(item, index) in ranking.filter(Boolean)"
-            v-bind:key="index"
-          >
-            <RankingTotalItem>
-              <template #index>{{ index + 1 }}.</template>
-              <template #playerName>
-                {{ item.first_name }} {{ item.last_name }}
-                <span v-if="item.is_cheater">
-                  <i class="fa-solid fa-poo"></i>
-                </span>
-              </template>
-              <template #coinsWonGame>
-                {{ item.coins_won_game.toFixed(2) }}
-              </template>
-              <template #coinsWonTournament>
-                {{ item.coins_won_tournament.toFixed(2) }}
-              </template>
-              <template #coinsWonTotal>
-                {{ item.coins_won_total.toFixed(2) }}
-              </template>
-            </RankingTotalItem>
-          </tr>
-        </table>
+  <div class="mt-8 px-4">
+    <div class="w-full max-w-7xl mx-auto">
+      <div class="bg-gray-800 rounded-lg shadow-xl p-6">
+        <div class="flex flex-col mb-4">
+          <h2 class="text-lg font-semibold text-white bg-gray-700 px-4 py-2 rounded w-full">
+            Tournament Total Ranking: {{ this.tournament ? this.tournament.name : "Unknown" }}
+          </h2>
+        </div>
+        <div class="overflow-x-auto">
+          <table class="w-full">
+            <thead>
+              <tr class="text-white">
+                <th class="py-1 text-left">#</th>
+                <th class="py-1 text-left">User</th>
+                <th class="py-1 text-right">Game Coins Won</th>
+                <th class="py-1 text-right">Tournament Coins Won</th>
+                <th class="py-1 text-right">Total Coins Won</th>
+                <th class="py-1 text-right">Gap to Leader</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(item, index) in ranking.filter(Boolean)" :key="index" class="border-t border-gray-700">
+                <td class="py-1 text-gray-400">{{ index + 1 }}.</td>
+                <td class="py-1 text-gray-400">
+                  {{ item.first_name }} {{ item.last_name }}
+                  <span v-if="item.is_cheater" class="ml-2 text-yellow-500">
+                    <i class="fa-solid fa-poo"></i>
+                  </span>
+                </td>
+                <td class="py-1 text-right" :class="{
+                  'text-red-500': item.coins_won_game < 0,
+                  'text-green-500': item.coins_won_game > 0,
+                  'text-gray-400': item.coins_won_game === 0
+                }">
+                  {{ item.coins_won_game.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).replace(/,/g, ' ') }}
+                </td>
+                <td class="py-1 text-right" :class="{
+                  'text-red-500': item.coins_won_tournament < 0,
+                  'text-green-500': item.coins_won_tournament > 0,
+                  'text-gray-400': item.coins_won_tournament === 0
+                }">
+                  {{ item.coins_won_tournament.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).replace(/,/g, ' ') }}
+                </td>
+                <td class="py-1 text-right" :class="{
+                  'text-red-500': item.coins_won_total < 0,
+                  'text-green-500': item.coins_won_total > 0,
+                  'text-gray-400': item.coins_won_total === 0
+                }">
+                  {{ item.coins_won_total.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).replace(/,/g, ' ') }}
+                </td>
+                <td class="py-1 text-right text-gray-400">
+                  {{ index > 0 ? (ranking[0].coins_won_total - item.coins_won_total).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).replace(/,/g, ' ') : '-' }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import RankingTotalItem from "@/components/RankingTotalItem.vue";
 export default {
-  components: { RankingTotalItem },
   props: ["tournament", "ranking"],
   methods: {},
   mounted() {
